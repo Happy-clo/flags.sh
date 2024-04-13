7ac4d8ca21a1595a4a5fc22dc85e08ccdcdcff4c
Happy-clo <support@wdsj.one>
2024-04-13 18:42:14 +0800

    更新网站页面，归档旧项目，添加翼龙标志，优化控制组对齐，重命名现代Java为现代向量，更新资金信息，创建许可证说明。

--- app/util/util.ts
+++ app/util/util.ts
@@ -5,6 +5,5 @@ export const SiteDetails = {
     "title": "flags.happys.icu",
     "description": "一个简单的脚本生成器，可使用最佳的Java标签启动 Minecraft 服务器。",
     "favicon": "/favicon.ico",
-    "logo": "/asset/logo.png",
-    }
+    "logo": "/asset/logo.png"
 };
