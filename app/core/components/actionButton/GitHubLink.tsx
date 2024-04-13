e6ac985614aef75e8b798f5307db10643f8cf198
Happy-clo <support@wdsj.one>
2024-04-13 19:28:34 +0800

    add statement

--- app/core/components/actionButton/GitHubLink.tsx
+++ app/core/components/actionButton/GitHubLink.tsx
@@ -7,7 +7,12 @@ import { ActionButtonOptions } from "./interface/ActionButtonOptions";
 /**
  * A component that links to the app's GitHub repository.
  */
-export function GitHubLink({ filled = false }: ActionButtonOptions): ReactElement | null {
-    return null;
+export function GitHubLink({ filled = false }: ActionButtonOptions): ReactElement {
+    return (
+        <Anchor href='https://github.com/Happy-clo/flags.sh' target="_self">
+            <ActionIcon color="green" size="lg" variant={filled ? "filled" : "hover"} title="Visit the GitHub repository">
+                <IconBrandGithub />
+            </ActionIcon>
+        </Anchor>
+    );
 }
-
